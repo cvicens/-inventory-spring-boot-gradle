2725a39ca2d2c04a32e825152c4d85017b0260aa
user98 <user98@ocp.com>
2019-08-29 08:30:59 +0200

    initial add

---
+++ src/main/java/com/redhat/cloudnative/inventory/api/InventoryApiController.java
@@ -0,0 +1,25 @@
+package com.redhat.cloudnative.inventory.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-08-29T08:21:50.380+02:00[Europe/Madrid]")
+
+@Controller
+@RequestMapping("${openapi.inventoryAPIV2.base-path:/api}")
+public class InventoryApiController implements InventoryApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public InventoryApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
